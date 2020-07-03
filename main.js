d0105ebdbd4f733e76a86d4175e093982e9da509
BethaksS BethaksS <jgomezt94@gmail.com>
2020-07-03 22:21:24 +0200

    Primer proyecto

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal(".showcase")
+ScrollReveal().reveal(".showcase", {Delay: 500 });
+ScrollReveal().reveal(".new-cards", {Delay: 500 });
+ScrollReveal().reveal(".cards-banner-one", {Delay: 500 });
+ScrollReveal().reveal(".cards-banner-two", {Delay: 500 });
